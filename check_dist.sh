d090c55a92f6779189fb729ba84942bc9f6daa2e
Yosef da Iry <yosefda@gmail.com>
2018-02-18 04:05:39 +1100

    added check_dist script

---
+++ check_dist.sh
@@ -0,0 +1,12 @@
+RED="\033[0;31m";
+GREEN="\033[0;32m";
+NC="\033[0m";
+
+DIST_FILE="dist/Recommendation.phar";
+if [ -f $DIST_FILE ]; then
+  echo "${GREEN}Successfully disting $DIST_FILE${NC}";
+  exit 0;
+else
+  echo "${RED}Failed to dist $DIST_FILE${NC}";
+  exit 1;
+fi
