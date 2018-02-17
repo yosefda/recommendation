fef3fc09eafc90b8f147fa9b44d2243c8cd741a6
Yosef da Iry <yosefda@gmail.com>
2018-02-18 04:09:31 +1100

    tidy up

--- check_dist.sh
+++ check_dist.sh
@@ -2,6 +2,7 @@ RED="\033[0;31m";
 GREEN="\033[0;32m";
 NC="\033[0m";
 
+echo "\n\n";
 DIST_FILE="dist/Recommendation.phar";
 if [ -f $DIST_FILE ]; then
   echo "${GREEN}Successfully disting $DIST_FILE${NC}";
